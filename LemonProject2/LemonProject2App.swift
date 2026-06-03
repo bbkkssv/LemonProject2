0e7b473d8d75cbe0ec3a03a0d8110004cdbdf967
Robert <bbkkssv@gmail.com>
2026-06-02 20:35:06 -0500

    Initial Commit

---
+++ LemonProject2/LemonProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonProject2App.swift
+//  LemonProject2
+//
+//  Created by Robert Vinson on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
