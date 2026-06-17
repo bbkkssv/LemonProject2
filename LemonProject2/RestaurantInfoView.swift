e736bd2e17ef21b69928cea3fa78af35963b123a
RobertVinson <bbkkssv@gmail.com>
2026-06-17 09:53:08 -0500

    Assignment 3

--- LemonProject2/RestaurantInfoView.swift
+++ LemonProject2/RestaurantInfoView.swift
@@ -14,11 +14,33 @@ struct RestaurantInfoView: View {
         "TikTok": "@restaurant.lemon"
     ]
 
+    let menuCategories = [
+        "Antipasti": "Starters",
+        "Primi": "Pasta & Risotto",
+        "Secondi": "Main Courses",
+        "Contorni": "Side Dishes",
+        "Dolci": "Desserts"
+    ]
+
     var body: some View {
         NavigationStack {
             List {
                 Section("Social Media") {
                     ForEach(Array(socialMedia), id: \.key) { (key, value) in
+                        HStack {
+                            Text(key)
+                                .foregroundColor(.blue)
+
+                            Spacer()
+
+                            Text(value)
+                                .font(.title3)
+                        }
+                    }
+                }
+
+                Section("Menu Categories") {
+                    ForEach(Array(menuCategories), id: \.key) { (key, value) in
                         HStack {
                             Text(key)
                                 .foregroundColor(.blue)
